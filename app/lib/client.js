31e2aa200a1742acfd01dee50e6571b06839368b
legends-killer <958775729ljz@gmail.com>
2021-08-15 16:50:14 +0800

    fix: client host config

--- app/lib/client.js
+++ app/lib/client.js
@@ -18,6 +18,7 @@ module.exports.createMqttClient = function(config, app) {
   assert(is.string(config.clientId), 'config.clientId must be String!');
 
   const mqttClient = mqtt.connect(config.host, {
+    host : config.host,
     protocol: 'mqtt',
     clientId: config.clientId,
     username: config.username,
